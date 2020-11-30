e3d55947a5df8e8654a94afaad3760f6f47f8278
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-11-30 15:50:23 -0300

    my first commit - implementation only springboot web - basic

---
+++ src/main/java/br/com/solucitiva/projetoestudospring/ProjetoEstudoSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.solucitiva.projetoestudospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEstudoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEstudoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solucitiva/projetoestudospring/ProjetoEstudoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solucitiva.projetoestudospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstudoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
